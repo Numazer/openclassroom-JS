5a663bd07b047acf1eda9256ce3676d2d6c667b8
numa <emmanuel.saadisarrault@gmail.com>
2024-10-21 12:17:37 +0200

    poj

--- index.js
+++ index.js
@@ -23,5 +23,9 @@ let score = 0
         afficherResultat(score, nbDeMotsProposés)
     }
 
-    let baliseZoneProposition = document.getElementById("zoneProposition");
-console.log(baliseZoneProposition);
+    let listeInputRadio = document.querySelectorAll(".zoneChoix input");
+console.log(listeInputRadio);
+
+for (let i = 0; i < listeInputRadio.length; i++) {
+    console.log(listeInputRadio[i]);
+}
